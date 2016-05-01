98d7ad39708297607fa751b2adfa0ec95b1865b8
Vadim Lavorchuk <lavorchukvadim@gmail.com>
2016-05-01 17:01:57 +0300

    unnamed
    поправил, сделал универсально... вне зависимости от количества вершин и
    граней

--- js/canvas.js
+++ js/canvas.js
@@ -2,48 +2,65 @@
  * Created by lavor on 01.05.2016.
  */
 window.onload = function () {
-    var top = [
-        {
-            x: 80,
-            y: 50,
-            name: 'First'
-        },
-        {
-            x: 25,
-            y: 80,
-            name: 'Second'
-        },
-        {
-            x: 110,
-            y: 220,
-            name: 'Third'
-        },
-        {
-            x: 220,
-            y: 110,
-            name: 'Four'
-        }
-    ],
+    var tops = [
+            {
+                x: 50,
+                y: 50,
+                name: 'First'
+            },
+            {
+                x: 280,
+                y: 100,
+                name: 'Second'
+            },
+            {
+                x: 50,
+                y: 280,
+                name: 'Third'
+            },
+            {
+                x: 280,
+                y: 280,
+                name: 'Four'
+            }
+        ],
+        edges = [
+            {
+                to: [1, 2, 3], // от нашей вершины (0) к другим
+                weight: [7, 19, 3] // веса от нашей вершины (0) к другим
+            },
+            {
+                to: [0, 2, 3],
+                weight: [7, 29, 9]
+            },
+            {
+                to: [0, 1, 3],
+                weight: [19, 29, 14]
+            },
+            {
+                to: [0, 1, 2],
+                weight: [3, 9, 14]
+            }
+        ],
         radius = 5, drag = false, currElement,
         canvas = document.getElementsByClassName('mainDraw')[0],
         canvasWidth = canvas.width,
         canvasHeight = canvas.height,
         ctx = canvas.getContext('2d');
-
+    drawAll();
     canvas.addEventListener('mousedown', function (event) {
         var dx, dy, item;
         dx = event.pageX - this.offsetLeft;
         dy = event.pageY - this.offsetTop;
-        for (item in top) {
-            if (dx >= top[item].x + radius || dx <= top[item].x - radius
+        for (item in tops) {
+            if (dx >= tops[item].x + radius || dx <= tops[item].x - radius
                 &&
-                dy >= top[item].y + radius || dy <= top[item].y - radius) {
+                dy >= tops[item].y + radius || dy <= tops[item].y - radius) {
                 drag = false;
                 currElement = '';
             } else {
                 drag = true;
                 currElement = item;
-                console.log(currElement);
                 return [currElement, drag];
             }
         }
@@ -54,58 +71,71 @@ window.onload = function () {
     });
     canvas.addEventListener('mousemove', function (event) {
         if (drag) {
-            top[currElement].x = event.pageX - this.offsetLeft;
-            top[currElement].y = event.pageY - this.offsetTop;
+            tops[currElement].x = event.pageX - this.offsetLeft;
+            tops[currElement].y = event.pageY - this.offsetTop;
             drawAll();
-            console.log('mouseMove');
         }
     });
-
-
-
-    drawAll();
-
-
     function drawAll() {
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-        drawCircle(top[0].x, top[0].y, top[0].name);
-        drawCircle(top[1].x, top[1].y, top[1].name);
-        drawCircle(top[2].x, top[2].y, top[2].name);
-        drawCircle(top[3].x, top[3].y, top[3].name);
-        drawLine(top[0].x, top[0].y, top[1].x, top[1].y, 4);
-        drawLine(top[0].x, top[0].y, top[2].x, top[2].y, 7);
-        drawLine(top[0].x, top[0].y, top[3].x, top[3].y, 11);
-        drawLine(top[1].x, top[1].y, top[3].x, top[3].y, 17);
-        drawLine(top[2].x, top[2].y, top[3].x, top[3].y, 3);
-        drawLine(top[1].x, top[1].y, top[2].x, top[2].y, 14);
+        var top = 0,
+            edge = 0,
+            count = 0,
+            line = {
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 0,
+                w: 0
+            };
+        for (top in tops) {
+            drawCircle(tops[top].x, tops[top].y, tops[top].name);
+        }
+        for (edge; edge <= edges.length; edge++) {
+            count = 0;
+            try {
+                for (count; count <= edges[edge].to.length; count++) {
+                    if (edge == count) {
+                        continue;
+                    }
+                    if (edge == 0 || edge == 1) {
+                        line.w = edges[edge].weight[count-1];
+                    } else {
+                        line.w = edges[edge].weight[count];
+                    }
+                    line.x1 = tops[edge].x;
+                    line.y1 = tops[edge].y;
+                    line.x2 = tops[count].x;
+                    line.y2 = tops[count].y;
+                    drawLine(line.x1, line.y1, line.x2, line.y2, line.w);
+                }
+            } catch (err) {} //иначе куча ошибок, а они вам надо?
+        }
     }
     function drawCircle(x, y, name) {
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, Math.PI*2, true);
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.8)';
-        ctx.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillStyle = 'rgba(0, 173, 255, 1)';
+        ctx.strokeStyle = 'rgba(0, 173, 255, 1)';
         ctx.fill();
         ctx.stroke();
         ctx.font = '12px serif';
         ctx.textAlign = 'center';
         ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-        ctx.fillText(name, x, y-10);
+        ctx.fillText(name, x, y-15);
         ctx.closePath();
     }
     function drawLine(x1, y1, x2, y2, weight) {
         var weight = weight || '';
         ctx.beginPath();
+        ctx.strokeStyle = 'rgba(0, 173, 255, .5)';
         ctx.moveTo(x1,y1);
         ctx.lineTo(x2,y2);
         ctx.stroke();
         ctx.font = '10px serif';
         ctx.textAlign = 'center';
-        ctx.fillStyle = 'rgba(0, 255, 0, 1)';
-        ctx.fillText(weight, (x1+x2)/2, (y1+y2)/2);
+        ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+        ctx.fillText(weight, ((x1+x2)/2)-5, ((y1+y2)/2)-5);
         ctx.closePath();
     }
-
-
-
-
-}
+};
